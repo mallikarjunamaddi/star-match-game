2be31cd0b0486f06caad5f218e0721beed80de5f
Mallikarjuna Maddi <mmaddi@microsoft.com>
2024-04-08 16:28:42 +0530

    Feat: Added StarDisplayArea Component.

---
+++ src/StarDisplayArea.jsx
@@ -0,0 +1,12 @@
+import utils from "./utils";
+import React from "react";
+
+function StarDisplayArea(props) {
+    return (
+        <>
+            {utils.range(1, props.starCount).map(i => <div className="star" key={i} />)}
+        </>
+    );
+}
+
+export default StarDisplayArea;
